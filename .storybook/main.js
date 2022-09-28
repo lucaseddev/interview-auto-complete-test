134d67bb4c1008cb399d0d9bbf85f6737e008872
Lucas Duarte <lucaseddev@gmail.com>
2022-09-28 13:58:32 -0300

    chore: configuring storybook and rollup

---
+++ .storybook/main.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+module.exports = {
+  stories: ["../src/**/*.stories.tsx"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-links",
+  ],
+  framework: "@storybook/react",
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader", "sass-loader"],
+      include: path.resolve(__dirname, "../src"),
+    });
+
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve("babel-loader"),
+      options: {
+        presets: [["react-app", { flow: false, typescript: true }]],
+      },
+    });
+    config.resolve.extensions.push(".ts", ".tsx", ".js", ".jsx");
+    config.resolve.modules.push(path.resolve(__dirname, "../src"));
+
+    return config;
+  },
+  reactOptions: {
+    fastRefresh: true,
+  },
+  core: {
+    builder: 'webpack5',
+  },
+};
